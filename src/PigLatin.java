98785e3e9c38e81c3521842cabaa01864b08497d
kanvar22 <Guest@wts6.attlocal.net>
2015-08-31 18:31:33 -0700

    new

---
+++ src/PigLatin.java
@@ -0,0 +1,17 @@
+
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class PigLatin {
+public static void main(String[] args) {
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JTextField input = new JTextField(20);
+JTextField output = new JTextField(20);
+JButton button = new JButton();
+}
+}
