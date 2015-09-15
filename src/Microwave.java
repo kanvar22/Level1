ba699bfce4e0eb18ddb0a6c3f1cfa679c3705de9
kanvar22 <Guest@wts11.attlocal.net>
2015-09-14 18:32:54 -0700

    new

---
+++ src/Microwave.java
@@ -0,0 +1,32 @@
+/*
+* Your mission should you choose to accept it:
+ *         Add a main method to the Popcorn class to cook a bag of Popcorn. Don't change the existing methods.
+ */
+
+class Microwave {
+	private int cookTime;
+	Popcorn thingToBeCooked;
+
+	Microwave() {
+		System.out.println("Microwave says: a Microwave has been made.");
+	}
+
+	void putInMicrowave(Popcorn thingToBeCooked) {
+		System.out.println("Microwave says: popcorn put in microwave.");
+		this.thingToBeCooked = thingToBeCooked;
+	}
+
+	void setTime(int cookTimeInMinutes) {
+		System.out.println("Microwave says: cook time is set to " + cookTime + " minutes.");
+		this.cookTime = cookTimeInMinutes;
+	}
+
+	void startMicrowave() {
+		if (thingToBeCooked == null)
+			System.out.println("Microwave says: there's nothing in the microwave!");
+		for (int i = 0; i < cookTime*10 + 1; i++) {
+			thingToBeCooked.applyHeat();
+		}
+	}
+}
+
--- src/Popcorn.java
+++ src/Popcorn.java
@@ -1,4 +1,39 @@
 
 public class Popcorn {
 
+	private int kernels = 20;
+	private String flavor;
+
+	Popcorn(String flavor) {
+		this.flavor = flavor;
+		System.out.println("Popcorn says: making package of " + this.flavor
+				+ " popcorn.");
+	}
+
+	public void applyHeat() {
+		pause();
+
+		if (kernels == 0) {
+			System.out.println("Popcorn says: Time to eat popcorn!");
+		} else {
+			System.out.println("POP!" + kernels);
+			kernels--;
+		}
+	}
+
+	private void pause() {
+		try {
+			Thread.sleep(150);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Microwave micro = new Microwave();
+		Popcorn pop = new Popcorn("butter");
+		micro.putInMicrowave(pop);
+		micro.setTime(60);
+		micro.startMicrowave();
+	}
 }
