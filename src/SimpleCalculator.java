84f8b2b5a7f172d596e1952ac8b9621856602d3b
Skylar <gerings17@bishops.com>
2016-08-01 18:32:32 -0700

    Simple Calculator

--- src/SimpleCalculator.java
+++ src/SimpleCalculator.java
@@ -1,55 +1,103 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class SimpleCalculator {
-	int text;
-	int text1;
+public class SimpleCalculator implements ActionListener {
 
-	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-		JTextField text = new JTextField();
-		JTextField text1 = new JTextField();
-		JButton bt1 = new JButton();
-		JButton bt2 = new JButton();
-		JButton bt3 = new JButton();
-		JButton bt4 = new JButton();
-		JLabel label = new JLabel();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JTextField textf = new JTextField();
+	JTextField textj = new JTextField();
+	JButton bt1 = new JButton();
+	JButton bt2 = new JButton();
+	JButton bt3 = new JButton();
+	JButton bt4 = new JButton();
+	JButton bt5 = new JButton();
+	JLabel label = new JLabel();
+
+	public SimpleCalculator() {
 		frame.add(panel);
-		panel.add(text);
-		panel.add(text1);
+		panel.add(textf);
+		panel.add(textj);
 		panel.add(bt1);
 		panel.add(bt2);
 		panel.add(bt2);
 		panel.add(bt3);
 		panel.add(bt4);
+		panel.add(bt5);
 		panel.add(label);
-		bt1.setText("add");
-		bt2.setText("sub");
-		bt3.setText("mul");
-		bt4.setText("div");
-		text.setColumns(4);
-		text1.setColumns(4);
+		bt1.setText("+");
+		bt2.setText("-");
+		bt3.setText("*");
+		bt4.setText("/");
+		bt5.setText(">");
+		bt1.addActionListener(this);
+		bt2.addActionListener(this);
+		bt3.addActionListener(this);
+		bt4.addActionListener(this);
+		bt5.addActionListener(this);
+		textf.setColumns(4);
+		textj.setColumns(4);
 		frame.setVisible(true);
 		frame.pack();
 	}
 
-	public int add(int text, int text1) {
+	public static void main(String[] args) {
+		SimpleCalculator calculator = new SimpleCalculator();
+
+	}
+
+	public double add(double text, double text1) {
 		return text + text1;
 	}
 
-	public int sub(int text, int text1) {
+	public double sub(double text, double text1) {
 		return text - text1;
 	}
 
-	public int mul(int text, int text1) {
+	public double mul(double text, double text1) {
 		return text * text1;
 	}
 
-	public int div(int text, int text1) {
+	public double div(double text, double text1) {
 		return text / text1;
+	}
+
+	public boolean grea(double text, double text1) {
+		return text > text1;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+		double a = Double.parseDouble(textf.getText());
+		double b = Double.parseDouble(textj.getText());
+		if (e.getSource() == bt1) {
+			double c = add(a, b);
+			label.setText("" + c);
+		}
+		if (e.getSource() == bt2) {
+			double d = sub(a, b);
+			label.setText("" + d);
+		}
+		if (e.getSource() == bt3) {
+			double f = mul(a, b);
+			label.setText("" + f);
+		}
+		if (e.getSource() == bt4) {
+			double g = div(a, b);
+			label.setText("" + g);
+		}
+		if (e.getSource() == bt5) {
+			boolean h = grea(a, b);
+			label.setText("" + h);
+		}
+
 	}
 }
